37bbff71cc453ba9ad9c9413765d6bc383739aba
Bosco Kwan <bkwan15@my.bcit.ca>
2019-03-06 13:55:56 -0800

    Create Courses.java

---
+++ src/learningmanagementsystem/Courses.java
@@ -0,0 +1,24 @@
+package learningmanagementsystem;
+
+public class Courses implements Tables{
+
+    @Override
+    public void add(){
+
+    }
+
+    @Override
+    public void update(){
+
+    }
+
+    @Override
+    public void delete(){
+
+    }
+
+    @Override
+    public void select() {
+
+    }
+}
