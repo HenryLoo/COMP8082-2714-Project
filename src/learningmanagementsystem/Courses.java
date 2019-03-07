ceec78a6de89804c1f9fc78c7602480947556216
Haonan Zhong <graham980410@gmail.com>
2019-03-06 19:39:11 -0800

    Add the delete feature

--- src/learningmanagementsystem/Courses.java
+++ src/learningmanagementsystem/Courses.java
@@ -1,5 +1,9 @@
 package learningmanagementsystem;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Courses implements Tables {
 
     @Override
@@ -15,6 +19,35 @@ public class Courses implements Tables {
 
     @Override
     public void delete() {
+        try {
+            //getting connection
+
+            conn = MyDBConnection.getConnection();
+            if (conn = null)
+                return;
+
+            //remind user to enter the user id they want to delete
+            System.out.print("Please enter the id of user to delete:");
+            Scanner input;
+            input = new Scanner(System.in);
+            int userid = input.nextInt();
+
+            //define the Sql statement
+            String deleteSql = "DELETE FROM login WHERE userid ="+userid+";";
+
+            //acquire the statement object
+            stt = conn.createStatement();
+
+            //execute the Sql statement
+            stt.executeUpdate(deleteSql);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try{
+                conn.close();
+            } catch (Exception e2){
+        }}
 
     }
 
