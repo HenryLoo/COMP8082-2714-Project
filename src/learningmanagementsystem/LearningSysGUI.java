3a21c0dfea1224fde3d0f28b66b8233bea2f07a5
Thomas Bui <thomasbui198@gmail.com>
2019-03-18 12:40:33 -0700

    Created the function buttons for the Course table

--- src/learningmanagementsystem/LearningSysGUI.java
+++ src/learningmanagementsystem/LearningSysGUI.java
@@ -1,31 +1,29 @@
 package learningmanagementsystem;
 
 import javafx.event.ActionEvent;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-import java.security.InvalidParameterException;
-import java.util.ArrayList;
-import java.util.Hashtable;
-
 public class LearningSysGUI extends GridPane {
 
-    // The options bar at the top of the screen.
-    private OptionsBar topBar;
+    // The option bar at the top of the screen.
+    private OptionBar topBar;
+
+    // The bar that displays table functions.
+    private HBox functionOptions;
 
     // The current pane being displayed.
     private Pane currentPane;
+
+    // The current table that user is looking at.
+    private Tables currentTable;
 
     // The ecosystem.
     private Session mySession;
@@ -33,11 +31,13 @@ public class LearningSysGUI extends GridPane {
 
     public LearningSysGUI(Session newSession) {
         mySession = newSession;
-        topBar = new OptionsBar();
+        topBar = new OptionBar();
         currentPane = greetingPane();
+        functionOptions = new HBox();
 
-        add(topBar.createOptionsBar(), 0, 0);
-        add(currentPane, 0, 1);
+        add(topBar.createTableNameBar(), 0, 0);
+        add(functionOptions, 0, 1);
+        add(currentPane, 0, 2);
 
         final int Hgap = 5;
         final int Vgap = 20;
@@ -53,7 +53,7 @@ public class LearningSysGUI extends GridPane {
 
     private VBox greetingPane() {
         Text welcome = new Text("Welcome to the Learning System!");
-        Text subtext = new Text("Choose an option above to get started.");
+        Text subtext = new Text("Choose a table above to get started.");
 
         welcome.setFont(Font.font(40));
         subtext.setFont(Font.font(30));
@@ -63,11 +63,13 @@ public class LearningSysGUI extends GridPane {
         return vbox;
     }
 
-    private class OptionsBar {
+    private class OptionBar {
 
-        // create the buttons at the top of the program
-        private HBox createOptionsBar() {
-            Button courseTable = createMyPoolButton();
+        /**
+         * Create the buttons at the top of the program
+          */
+        public HBox createTableNameBar() {
+            Button courseTable = createCoursesButton();
 
             final int gap = 10;
             HBox hbox = new HBox(courseTable);
@@ -76,16 +78,51 @@ public class LearningSysGUI extends GridPane {
             return hbox;
         }
 
-        private Button createMyPoolButton() {
+        private Button createCoursesButton() {
             Button myButton = new Button("Courses");
 
             Tooltip tip = new Tooltip("View and edit the courses");
             myButton.setTooltip(tip);
+
+            myButton.setOnAction(this::createFunctionOptions);
             return myButton;
         }
 
-    }
+        // when a table is chosen, displays the functions
+        private void createFunctionOptions(ActionEvent event) {
+            String buttonName = findButtonName(event);
 
+            // equal to select command in sql
+            Button viewButton = new Button("View Table");
+
+            // equal to add command in sql
+            Button addButton = new Button("Add" + buttonName);
+
+            // equal to update command in sql
+            Button updateButton = new Button("Update" + buttonName);
+
+            // equal to delete command in sql
+            Button deleteButton = new Button("Delete" + buttonName);
+
+            final int gap = 5;
+            HBox hbox = new HBox(viewButton, addButton, updateButton, deleteButton);
+            hbox.setSpacing(gap);
+            hbox.setStyle("-fx-background-color: gray");
+
+            // get the functionOptions that's currently empty and set it to hbox.
+            functionOptions.getChildren().setAll(hbox);
+        }
+
+        public String findButtonName(ActionEvent event) {
+            // split the object text into two parts by separating using the quote symbol.
+            // to check why this work, you can use System.out.println(event.getSource()).
+            String[] buttonObj = event.getSource().toString().split("'");
+
+            final int indexContainingTheName = 1;
+            return buttonObj[indexContainingTheName];
+        }
+
+    }
 
 
 
