bef3d3ebfd049d837c7e7a357dc3ca5cb74a2d18
Thomas Bui <thomasbui198@gmail.com>
2019-03-18 18:11:40 -0700

    Tried to make inner classes

--- src/learningmanagementsystem/Courses.java
+++ src/learningmanagementsystem/Courses.java
@@ -12,7 +12,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Courses implements Tables {
+public class Courses extends Table {
 
     // the connection to the database
     private Connection myConn;
@@ -33,50 +33,159 @@ public class Courses implements Tables {
      */
     public Courses(Connection newMyConn) {
         myConn = newMyConn;
-        userMessage = new Label("");
-        userMessage.setFont(Font.font(13));
-        userMessage.setTextFill(Color.RED);
-    }
-
-    /**
-     * Create a grid pane containing elements needed to add courses.
-     * @return a GridPane with all the text fields.
-     */
-    public GridPane createAddDashBoard() {
-
-        Label functionTitle = new Label("Please enter the new course's data:");
-        Label courseIdLbl = new Label("CourseID: ");
-        Label courseNameLbl = new Label("Course Name: ");
-        Label courseProfLbl = new Label("Course Professor ID: ");
-        Label courseDescriptionLbl = new Label("Course Description: ");
-
-        functionTitle.setFont(Font.font(22));
-        courseIdLbl.setFont(Font.font(18));
-        courseNameLbl.setFont(Font.font(18));
-        courseProfLbl.setFont(Font.font(18));
-        courseDescriptionLbl.setFont(Font.font(18));
 
         courseIdTxtFld = new TextField();
         courseNameTxtFld = new TextField();
         courseProfTxtFld = new TextField();
         courseDescriptionTxtFld = new TextField();
 
+        userMessage = new Label("");
+        userMessage.setFont(Font.font(13));
+        userMessage.setTextFill(Color.RED);
+    }
+
+    private class AddDashBoard extends GridPane {
+        /**
+         * Create a grid pane containing elements needed to add courses.
+         * @return a GridPane with all the text fields.
+         */
+        public AddDashBoard() {
+            Label functionTitle = new Label("Please enter the new course's data:");
+            Label courseIdLbl = new Label("CourseID: ");
+            Label courseNameLbl = new Label("Course Name: ");
+            Label courseDescriptionLbl = new Label("Course Description: ");
+            Label courseProfLbl = new Label("Course Professor ID: ");
+
+            functionTitle.setFont(Font.font(22));
+            courseIdLbl.setFont(Font.font(18));
+            courseNameLbl.setFont(Font.font(18));
+            courseProfLbl.setFont(Font.font(18));
+            courseDescriptionLbl.setFont(Font.font(18));
+
+            add(functionTitle, 0, 0, 2, 1);
+            add(courseIdLbl, 0, 1);
+            add(courseNameLbl, 0, 2);
+            add(courseDescriptionLbl, 0, 3);
+            add(courseProfLbl, 0, 4);
+            add(userMessage, 0, 5, 2, 1);
+
+            add(courseIdTxtFld, 1, 1);
+            add(courseNameTxtFld, 1, 2);
+            add(courseDescriptionTxtFld, 1, 3);
+            add(courseProfTxtFld, 1, 4);
+
+            Button addBtn = new Button("Add Course");
+            addBtn.setOnAction(this::checkInputForAddingData);
+            add(addBtn, 0, 6);
+
+            final int hGap = 5;
+            final int vGap = 10;
+            setHgap(hGap);
+            setVgap(vGap);
+        }
+
+        // try to add data
+        // if there are errors, let users know
+        private void checkInputForAddingData(ActionEvent event) {
+            // turn off the error indicator
+            inputErrorIndicator = false;
+
+            // create an error message for user
+            String message = "";
+            int courseProfId = 0;
+
+            String courseId = courseIdTxtFld.getText().trim();
+            if (!checkCourseID(courseId)) {
+                message += "Your course id must be six characters long. \n"
+                        + "It must start with three letters and end with three digits. \n";
+                inputErrorIndicator = true;
+                courseIdTxtFld.setStyle("-fx-border-color: red");
+            }
+
+            String courseName = courseNameTxtFld.getText().trim();
+            if (!checkCourseName(courseName)) {
+                message += "Your course name must be less than 40 characters. \n";
+                inputErrorIndicator = true;
+                courseIdTxtFld.setStyle("-fx-border-color: red");
+            }
+
+            try {
+                courseProfId = Integer.parseInt(courseProfTxtFld.getText().trim());
+                if (!checkProfID(courseProfId)) {
+                    message += "The professor ID doesn't exist. \n";
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                inputErrorIndicator = true;
+                courseProfTxtFld.setStyle("-fx-border-color: red");
+            }
+
+            String courseDescription = courseDescriptionTxtFld.getText().trim();
+            if (!checkDescription(courseDescription)) {
+                message += "Your course description must be less than 150 characters. \n";
+                inputErrorIndicator = true;
+                courseIdTxtFld.setStyle("-fx-border-color: red");
+            }
+
+            if (inputErrorIndicator) {
+                userMessage.setTextFill(Color.RED);
+                userMessage.setText(message);
+            } else {
+                add(courseId, courseName, courseDescription, courseProfId);
+            }
+        }
+
+        /**
+         * Add data to the Courses table.
+         * @param courseID
+         * @param name
+         * @param description
+         * @param profID
+         */
+        public void add(String courseID, String name, String description, int profID){
+
+            String sql = "INSERT INTO Courses VALUES('" + courseID + "', '"+ name + "', '"
+                    + description + "', " + profID + ");";
+
+            try {
+                Statement newCommand = myConn.createStatement();
+                newCommand.executeUpdate(sql);
+                newCommand.close();
+
+                // let user know course is added
+                userMessage.setTextFill(Color.GREEN);
+                userMessage.setText("Course Added.");
+
+                //
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * Create a grid pane containing elements needed to search courses.
+     * @return a GridPane with all the textfields.
+     */
+    public GridPane createSearchDashBoard() {
+
+        Label functionTitle = new Label("Search for Courses By CourseId:");
+        Label courseIdLbl = new Label("CourseID: ");
+
+        functionTitle.setFont(Font.font(22));
+        courseIdLbl.setFont(Font.font(18));
+
         GridPane gp = new GridPane();
 
         gp.add(functionTitle, 0, 0, 2, 1);
         gp.add(courseIdLbl, 0, 1);
-        gp.add(courseNameLbl, 0, 2);
-        gp.add(courseProfLbl, 0, 3);
-        gp.add(courseDescriptionLbl, 0, 4);
         gp.add(userMessage, 0, 5, 2, 1);
 
         gp.add(courseIdTxtFld, 1, 1);
-        gp.add(courseNameTxtFld, 1, 2);
-        gp.add(courseProfTxtFld, 1, 3);
-        gp.add(courseDescriptionTxtFld, 1, 4);
 
-        Button addBtn = new Button("Add Course");
-        addBtn.setOnAction(this::checkInputForAddingData);
+        Button addBtn = new Button("Search Course");
         gp.add(addBtn, 0, 6);
 
         final int hGap = 5;
@@ -87,56 +196,21 @@ public class Courses implements Tables {
         return gp;
     }
 
-    // try to add data
-    // if there are errors, let users know
-    private void checkInputForAddingData(ActionEvent event) {
-        // turn off the error indicator
-        inputErrorIndicator = false;
+    // create the search result area
+    private GridPane createSearchResultArea() {
+        Label courseIdLbl = new Label("CourseID");
+        Label courseNameLbl = new Label("Course Name: ");
+        Label courseDescriptionLbl = new Label("Description: ");
+        Label courseProfLbl = new Label("Professor ID: ");
 
-        // create an error message for user
-        String message = "";
-        int courseProfId = 0;
+        GridPane gp = new GridPane();
 
-        String courseId = courseIdTxtFld.getText().trim();
-        if (!checkCourseID(courseId)) {
-            message += "Your course id must be six characters long. \n"
-                    + "It must start with three letters and end with three digits. \n";
-            inputErrorIndicator = true;
-            courseIdTxtFld.setStyle("-fx-border-color: red");
-        }
+        gp.add(courseIdLbl, 0, 0);
+        gp.add(courseNameLbl, 1, 0);
+        gp.add(courseDescriptionLbl, 2, 0);
+        gp.add(courseProfLbl, 3, 0);
 
-        String courseName = courseNameTxtFld.getText().trim();
-        if (!checkCourseName(courseName)) {
-            message += "Your course name must be less than 40 characters. \n";
-            inputErrorIndicator = true;
-            courseIdTxtFld.setStyle("-fx-border-color: red");
-        }
-
-        try {
-            courseProfId = Integer.parseInt(courseProfTxtFld.getText().trim());
-            if (!checkProfID(courseProfId)) {
-                message += "The professor ID doesn't exist. \n";
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            inputErrorIndicator = true;
-            courseProfTxtFld.setStyle("-fx-border-color: red");
-        }
-
-        String courseDescription = courseDescriptionTxtFld.getText().trim();
-        if (!checkDescription(courseDescription)) {
-            message += "Your course description must be less than 150 characters. \n";
-            inputErrorIndicator = true;
-            courseIdTxtFld.setStyle("-fx-border-color: red");
-        }
-
-        if (inputErrorIndicator) {
-            userMessage.setTextFill(Color.RED);
-            userMessage.setText(message);
-        } else {
-            add(courseId, courseName, courseDescription, courseProfId);
-        }
-
+        return gp;
     }
 
     /**
@@ -144,8 +218,7 @@ public class Courses implements Tables {
      * @param colName the column name as a String
      * @parem value the value as a String
      */
-    @Override
-    public void select(String colName, String value) {
+    public void search(String colName, String value) {
         String sql = "SELECT * FROM Courses WHERE " + colName + " = " + value;
         try {
             Statement newCommand = myConn.createStatement();
@@ -160,36 +233,6 @@ public class Courses implements Tables {
 
     }
 
-    /**
-     * Add data to the Courses table.
-     * @param courseID
-     * @param name
-     * @param description
-     * @param profID
-     */
-    @Override
-    public void add(String courseID, String name, String description, int profID){
-
-        String sql = "INSERT INTO Courses VALUES('" + courseID + "', '"+ name + "', '"
-                + description + "', " + profID + ");";
-
-        try {
-            Statement newCommand = myConn.createStatement();
-            newCommand.executeUpdate(sql);
-            newCommand.close();
-
-            // let user know course is added
-            userMessage.setTextFill(Color.GREEN);
-            userMessage.setText("Course Added.");
-
-            //
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
     public void delete() {
 
         try {
@@ -222,7 +265,6 @@ public class Courses implements Tables {
 
     }
 
-    @Override
     public void update() {
 
     }
---
+++ src/learningmanagementsystem/Table.java
@@ -0,0 +1,4 @@
+package learningmanagementsystem;
+
+abstract public class Table {
+}
